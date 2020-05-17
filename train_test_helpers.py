942e18b632ccfe8eef86058e013cd667b8a5b25a
rednotion <elizabethlimsm@gmail.com>
2020-05-17 12:39:54 +0000

    add files

---
+++ train_test_helpers.py
@@ -0,0 +1,90 @@
+import numpy as np
+import torch
+
+def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path):
+    """returns trained model"""
+    # initialize tracker for minimum validation loss
+    valid_loss_min = np.Inf 
+    
+    for epoch in range(1, n_epochs+1):
+        # initialize variables to monitor training and validation loss
+        train_loss = 0.0
+        valid_loss = 0.0
+        
+        ###################
+        # train the model #
+        ###################
+        model.train()
+        for batch_idx, (data, target) in enumerate(loaders['train']):
+            # move to GPU
+            if use_cuda:
+                data, target = data.cuda(), target.cuda()
+            ## find the loss and update the model parameters accordingly
+            ## record the average training loss, using something like
+            ## train_loss = train_loss + ((1 / (batch_idx + 1)) * (loss.data - train_loss))
+            output = model(data)
+            loss = criterion(output, target)
+            train_loss = train_loss + ((1 / (batch_idx + 1)) * (loss.data - train_loss))
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+        ######################    
+        # validate the model #
+        ######################
+        model.eval()
+        for batch_idx, (data, target) in enumerate(loaders['valid']):
+            # move to GPU
+            if use_cuda:
+                data, target = data.cuda(), target.cuda()
+            ## update the average validation loss
+            output = model(data)
+            loss = criterion(output, target)
+            valid_loss = valid_loss + ((1 / (batch_idx + 1)) * (loss.data - valid_loss))
+            
+        # print training/validation statistics 
+        print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
+            epoch, 
+            train_loss,
+            valid_loss
+            ))
+        
+        ## TODO: save the model if validation loss has decreased
+        if valid_loss < valid_loss_min:
+            print('..Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(
+                valid_loss_min, valid_loss))
+            torch.save(model.state_dict(), save_path)
+            valid_loss_min = valid_loss
+            
+    # return trained model
+    return model
+
+def test(loaders, model, criterion, use_cuda):
+
+    # monitor test loss and accuracy
+    test_loss = 0.
+    correct = 0.
+    total = 0.
+
+    model.eval()
+    for batch_idx, (data, target) in enumerate(loaders['test']):
+        # move to GPU
+        if use_cuda:
+            data, target = data.cuda(), target.cuda()
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = model(data)
+        # calculate the loss
+        loss = criterion(output, target)
+        # update average test loss 
+        test_loss = test_loss + ((1 / (batch_idx + 1)) * (loss.data - test_loss))
+        # convert output probabilities to predicted class
+        pred = output.data.max(1, keepdim=True)[1]
+        # compare predictions to true label
+        correct += np.sum(np.squeeze(pred.eq(target.data.view_as(pred))).cpu().numpy())
+        total += data.size(0)
+            
+    print('Test Loss: {:.6f}\n'.format(test_loss))
+
+    print('\nTest Accuracy: %2d%% (%2d/%2d)' % (
+        100. * correct / total, correct, total))
